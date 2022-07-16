4f9747daa2043b9f5bd7d98fc95f5b670416c558
cassiocoltri <cassiocoltri@gmail.com>
2022-07-16 16:15:15 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <> {/* Como ele executa apenas UMA TAG, se coloca o "<>" e os demais codigos dentro dele */}
+    <ToastContainer />
       <Header />
       <main>
         <section id="sales">
